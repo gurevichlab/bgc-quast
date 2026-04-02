df23eff6fe6d94b2c7e496459dfd8a9a59969abf
Aleks0809 <alex.kushnareva99@gmail.com>
2026-04-02 03:42:40 +0200

    Script for metrics table description

---
+++ dev/metrics_table_description.py
@@ -0,0 +1,27 @@
+from src.reporting.report_config import ReportConfigManager
+
+config_manager = ReportConfigManager()
+
+report_modes = [
+    ("compare-samples", config_manager.get_config("basic_report")),
+    ("compare-to-reference", config_manager.get_config("compare_to_reference")),
+    ("compare-tools", config_manager.get_config("compare_tools")),
+]
+
+metrics_table = ['### Metrics description']
+
+metrics_table.append(
+    "metric | description | analysis mode\n"
+    "-------|-------------|----------------"
+)
+
+for mode_name, report in report_modes:
+    for metric in report.metrics:
+        metrics_table.append(
+            f'`{metric.display_name}` | {metric.description} | `{mode_name}`'
+        )
+
+metrics_md = "\n".join(metrics_table)
+
+with open("metrics_description.md", "w", encoding="utf-8") as f:
+    f.write(metrics_md)
